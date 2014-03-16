4509667711f6499e22134ec7f0717fa36cb5f2a9
Milton Plotkin <plotkinmilton@gmail.com>
2014-03-16 16:10:25 +1100

    Milton_16-Mar-2014
    * Created GitHub repository for the programmers, named with the working
    title "Swingers".
    * Created basic Platform Game engine to be built upon and discussed in
    future meetings.
    * Created a game level that features slopes, falls and walls for testing
    the most common and basic functionality that is anticipated to be
    present in the final product.
    * Added visual representation of the characters status to aid in
    debugging.

---
+++ Swingers/Assets/Scripts/Character.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * Base class of all player controlled characters?
+ */
+
+using UnityEngine;
+using System.Collections;
+
+// Prints an error if there is no CharacterController for this object.
+[RequireComponent( typeof(CharacterController))]
+
+public class Character : MonoBehaviour {
+
+	public float moveSpeed = 10.0f;		// Maximum speed the character reaches while running.
+
+	private float m_direction = 1;		// Can equal 1 or -1. 1 = Facing right.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Temporary: Move the character forard.
+		CharacterController controller = GetComponent<CharacterController>();
+		Vector3 forward = new Vector3 (1.0f * m_direction, 0.0f, 0.0f);
+		controller.SimpleMove (forward * moveSpeed * Time.deltaTime);
+
+		// Temporary: Show when the character is on the ground.
+		if (controller.isGrounded)
+			renderer.material.color = new Color(0.5f,0.0f,0.5f);
+		else
+			renderer.material.color = new Color(1.0f,1.0f,1.0f);
+	}
+}
